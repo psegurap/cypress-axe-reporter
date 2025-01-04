8c1790a940e620187603d536ad97a2041c375c67
Pedro Segura <psegurap01@gmail.com>
2025-01-04 16:06:56 -0500

    Minor changes to files distribution

--- plugin.js
+++ plugin.js
@@ -1,12 +1,11 @@
-const beforeRunHook = require("./lib/beforeRunHook")
-const afterRunHook = require("./lib/afterRunHook")
+const { beforeRunHook, afterRunHook } = require("./lib");
 
 module.exports = function (on) {
-  on('before:run', async (details) => {
-    await beforeRunHook(details);
-  });
+    on("before:run", async (details) => {
+        await beforeRunHook(details);
+    });
 
-  on('after:run', async (results) => {
-    await afterRunHook(results);
-  });
+    on("after:run", async (results) => {
+        await afterRunHook(results);
+    });
 };
