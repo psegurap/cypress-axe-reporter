4a34bb92831060d6fbf4e53bae7ca28b5ab4c5c7
Pedro Segura <51924485+psegurap@users.noreply.github.com>
2025-01-13 22:54:52 -0500

    added saveHtml and re-write defaultReporterOptions

--- register.js
+++ register.js
@@ -1,46 +1,47 @@
-let { defaultJSONA11yOutputFolder } = require("./lib/consts");
+let drOpts = require("./lib/defaultReporterOptions");
 let path = require("path");
-
 let reporterOptions = Cypress.config("reporterOptions");
-console.log(Cypress.config());
 
 if (reporterOptions && reporterOptions.reportDir) {
-    defaultJSONA11yOutputFolder = reporterOptions.reportDir + "/json/";
+    drOpts.reportDir = reporterOptions.reportDir;
 }
-
-console.log(defaultJSONA11yOutputFolder);
 
 const cypressAxeReporterCallBack = (violations) => {
     let file_name = `${
         Cypress.spec.baseName
     } - ${Cypress.currentTest.title.replace(/[^a-zA-Z ]/g, "")}.json`;
 
-    cy.writeFile(path.join("./", defaultJSONA11yOutputFolder, file_name), {
-        test_info: {
-            baseName: Cypress.spec.baseName,
-            fileName: Cypress.spec.fileName,
-            relative_path: Cypress.spec.relative,
-            spec_title: Cypress.currentTest.titlePath[0],
-            test_title: Cypress.currentTest.title,
-        },
-        a11y: {
-            url: window.location.origin,
-            violations: violations.map(
-                ({ id, impact, description, help, helpUrl, nodes }) => ({
-                    id,
-                    impact,
-                    description,
-                    help,
-                    helpUrl,
-                    nodes: nodes.map(({ html, target, failureSummary }) => ({
-                        html,
-                        target,
-                        failureSummary,
-                    })),
-                })
-            ),
-        },
-    });
+    cy.writeFile(
+        path.join("./", drOpts.reportDir, drOpts.a11yJsonDir, file_name),
+        {
+            test_info: {
+                baseName: Cypress.spec.baseName,
+                fileName: Cypress.spec.fileName,
+                relative_path: Cypress.spec.relative,
+                spec_title: Cypress.currentTest.titlePath[0],
+                test_title: Cypress.currentTest.title,
+            },
+            a11y: {
+                url: window.location.origin,
+                violations: violations.map(
+                    ({ id, impact, description, help, helpUrl, nodes }) => ({
+                        id,
+                        impact,
+                        description,
+                        help,
+                        helpUrl,
+                        nodes: nodes.map(
+                            ({ html, target, failureSummary }) => ({
+                                html,
+                                target,
+                                failureSummary,
+                            })
+                        ),
+                    })
+                ),
+            },
+        }
+    );
 };
 
 Cypress.Commands.add("cypressAxeReporterCallBack", cypressAxeReporterCallBack);
