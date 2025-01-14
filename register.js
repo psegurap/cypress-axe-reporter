2313b2273c1a42dbf0069ea44e0a5d85fc6294e3
Pedro Segura <51924485+psegurap@users.noreply.github.com>
2025-01-14 15:54:04 -0500

    Minor changes

--- register.js
+++ register.js
@@ -11,37 +11,39 @@ const cypressAxeReporterCallBack = (violations) => {
         Cypress.spec.baseName
     } - ${Cypress.currentTest.title.replace(/[^a-zA-Z ]/g, "")}.json`;
 
-    cy.writeFile(
-        path.join("./", drOpts.reportDir, drOpts.a11yJsonDir, file_name),
-        {
-            test_info: {
-                baseName: Cypress.spec.baseName,
-                fileName: Cypress.spec.fileName,
-                relative_path: Cypress.spec.relative,
-                spec_title: Cypress.currentTest.titlePath[0],
-                test_title: Cypress.currentTest.title,
-            },
-            a11y: {
-                url: window.location.origin,
-                violations: violations.map(
-                    ({ id, impact, description, help, helpUrl, nodes }) => ({
-                        id,
-                        impact,
-                        description,
-                        help,
-                        helpUrl,
-                        nodes: nodes.map(
-                            ({ html, target, failureSummary }) => ({
-                                html,
-                                target,
-                                failureSummary,
-                            })
-                        ),
-                    })
-                ),
-            },
-        }
-    );
+    if(Cypress.config("reporter") === "cypress-axe-reporter") {
+        cy.writeFile(
+            path.join("./", drOpts.reportDir, drOpts.a11yJsonDir, file_name),
+            {
+                test_info: {
+                    baseName: Cypress.spec.baseName,
+                    fileName: Cypress.spec.fileName,
+                    relative_path: Cypress.spec.relative,
+                    spec_title: Cypress.currentTest.titlePath[0],
+                    test_title: Cypress.currentTest.title,
+                },
+                a11y: {
+                    url: window.location.origin,
+                    violations: violations.map(
+                        ({ id, impact, description, help, helpUrl, nodes }) => ({
+                            id,
+                            impact,
+                            description,
+                            help,
+                            helpUrl,
+                            nodes: nodes.map(
+                                ({ html, target, failureSummary }) => ({
+                                    html,
+                                    target,
+                                    failureSummary,
+                                })
+                            ),
+                        })
+                    ),
+                },
+            }
+        );
+    }
 };
 
 Cypress.Commands.add("cypressAxeReporterCallBack", cypressAxeReporterCallBack);
